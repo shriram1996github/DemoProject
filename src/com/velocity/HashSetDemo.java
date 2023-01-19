f2273a06d7b2e6fee2b8e0fcb5bf8bf6886ab76b
shriram <shriram.doifode1996@gmail.com>
2023-01-19 23:56:17 +0530

    HashSetDemo classAdded

---
+++ src/com/velocity/HashSetDemo.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+		Set<String> set = new HashSet<String>();
+		set.add("ram");
+		set.add("vishal");
+		System.out.println(set);
+	}
+
+}
